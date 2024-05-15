29bf93f74e74e27c28b9389c15b1cf6eacafc5a8
AllanUng <1221107307@student.mmu.edu.my>
2024-05-15 18:34:57 +0800

    Login system first stage still working on buttons. Moving to account regristration after that

--- testing login.py
+++ testing login.py
@@ -33,6 +33,7 @@ class AnimatedGIF(tk.Label):
        
 root = tk.Tk()
 root.state('zoomed')
+root.geometry('935x500+300+200')
 root.configure(bg='#fff')
 root.resizable(False,False)
 gif_label = AnimatedGIF(root, "Undefined - Imgur.gif", 100)
@@ -62,7 +63,7 @@ def signin():
         messagebox.showerror("Invalid","Invalid username")
     
 frame=Frame(root,width=350,height=350,bg='white')
-frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+frame.place(x=480, y=70)
 
 heading=Label(frame,text='Sign in',fg='#57a1f8',bg='white',font=('Microsoft YahHei UI Light',23,'bold'))
 heading.place(x=130, y=5)
@@ -77,7 +78,7 @@ def on_leave(e):
     if name=='':
         user.insert(0, 'Username')
 
-user = Entry(frame,width=25, fg='black',border=0, bg='white',highlightthickness=0,font=('Microsoft YahHei UI Light',11))
+user = Entry(frame,width=25, fg='black',border=0, bg='white',font=('Microsoft YahHei UI Light',11))
 user.place(x=30, y=80)
 user.insert(0,'Username')
 user.bind('<FocusIn>',on_enter)
@@ -95,7 +96,7 @@ def on_leave(e):
     if pw=='':
         password.insert(0, 'Password')
 
-password = Entry(frame,width=25, fg='black',border=0, bg='white',highlightthickness=0,font=('Microsoft YahHei UI Light',11))
+password = Entry(frame,width=25, fg='black',border=0, bg='white',font=('Microsoft YahHei UI Light',11))
 password.place(x=30, y=150)
 password.insert(0,'Password')
 password.bind('<FocusIn>',on_enter)
@@ -105,7 +106,7 @@ Frame(frame,width=295,height=2,bg='black').place(x=25,y=177)
 
 ##############################################################################################################
 
-Button(frame,width=20,pady=7,text='Sign in',bg='#57a1f8',fg='white',border=0, command=signin).place(x=70,y=204)
+Button(frame,width=39,pady=7,text='Sign in',bg='#57a1f8',fg='white',border=0, command=signin).place(x=35,y=204)
 
 Label=Label(frame,text="Don't have an account?",fg='black',bg='white',font=('Microsoft YaHei UI Light',9))
 Label.place(x=75,y=270)
